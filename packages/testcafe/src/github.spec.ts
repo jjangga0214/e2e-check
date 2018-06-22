0a72e2f0b8b857c3a281c612ba83028477a61480
Philipp Zins <philipp.zins@mercateo.com>
2018-06-22 10:51:24 +0200

    added testcafe example

---
+++ packages/testcafe/src/github.spec.ts
@@ -0,0 +1,11 @@
+import { Selector } from 'testcafe';
+
+fixture('Github').page('https://github.com');
+
+test('check', async (t) => {
+  await t
+    .expect(Selector('title').innerText)
+    .contains('leading software development platform')
+    .expect(Selector('.js-signup-form button').innerText)
+    .eql('Sign up for GitHub');
+});
---
+++ packages/testcafe/src/google.spec.ts
@@ -0,0 +1,20 @@
+import { Selector } from 'testcafe';
+
+fixture('Google').page('https://www.google.com/ncr');
+
+test('check', async (t) => {
+  await t
+    .expect(Selector('title').innerText)
+    .eql('Google')
+    .expect(Selector('input[name="btnK"]').value)
+    .eql('Google Search');
+});
+
+test('search', async (t) => {
+  await t
+    .typeText(Selector('input[name="q"]'), 'e2e-check')
+    .click(Selector('body')) // closes overlay
+    .click(Selector('input[name="btnK"]'))
+    .expect(Selector('title').innerText)
+    .eql('e2e-check - Google Search');
+});
